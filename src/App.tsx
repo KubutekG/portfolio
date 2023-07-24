0036330bc1d7f452fc89864f752f2f2046325d21
KubutekG <jakubgiedzicz@gmail.com>
2023-07-24 17:26:58 +0200

    bit of styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import { useTranslation } from 'react-i18next'
-import LanguageSelector from './components/LanguageSelector'
-import './App.css'
+import { useTranslation } from "react-i18next";
+import "../styles/main.css";
 
 function App() {
-  const { t } = useTranslation()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { t } = useTranslation();
 
   return (
-    <>
-    <LanguageSelector />
-      {t("intro")}
-    </>
-  )
+    <main>
+      <button className="menu-button" aria-label="Open menu">
+        Menu
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+      <section></section>
+    </main>
+  );
 }
 
-export default App
+export default App;
