21c1a8e2d3bac2822bf2fded4a646fb8bffdcba6
KubutekG <jakubgiedzicz@gmail.com>
2023-07-26 14:05:00 +0200

    crud img alteration algorythm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-import { useTranslation } from "react-i18next";
+/* import { useTranslation } from "react-i18next"; */
+import { useEffect, useRef } from "react";
 import "../styles/main.css";
 
 function App() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { t } = useTranslation();
+  /* const { t } = useTranslation(); */
+  const wrap = useRef(null)
+  const img = useRef(null);
+  let scale = 1;
+  let img_position = 50;
+  let width = 100
+  let wrap_position = 0
+  const WRAP_POS_SPEED = 10
+  const WIDTH_SPEED = 5
+  const IMG_POS_SPEED = 2
+  const SCALE_SPEED = 0.025;
+
+  useEffect(() => {
+    function handleScroll(e: WheelEvent) {
+      if (e.deltaY > 0) {
+        if (scale > 0.7){
+          wrap.current.style.transform = `scale(${(scale -= SCALE_SPEED)})`;
+        }
+        if (width > 40){
+          wrap.current.style.width = `${width -= WIDTH_SPEED}%`
+        }
+        if (img_position > 0){
+          img.current.style.objectPosition = `0px ${img_position -= IMG_POS_SPEED}%`
+        }
+        if (scale.toFixed(1) === '0.7' && width === 40) {
+          console.log('aha')
+          wrap.current.style.transform = `translate(0px, ${wrap_position += WRAP_POS_SPEED}px)`
+        }
+      }
+    }
+    document.addEventListener("wheel", handleScroll);
+    return () => window.removeEventListener('wheel', handleScroll)
+  }, []);
 
   return (
     <main>
@@ -23,7 +55,11 @@ function App() {
           />
         </svg>
       </button>
-      <section></section>
+      <section className="intro">
+        <div className="intro-img-wrap" ref={wrap}>
+          <img src="/public/dummy.jpg" className="intro-img" ref={img}/>
+        </div>
+      </section>
     </main>
   );
 }
