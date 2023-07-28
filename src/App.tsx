60454e788023669dd127171f11de6be8bae71157
KubutekG <jakubgiedzicz@gmail.com>
2023-07-28 12:20:00 +0200

    refactored algorythm, switched to distancefromtop dependency

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,25 @@ function App() {
   /* const { t } = useTranslation(); */
   const wrap = useRef<HTMLDivElement>(null)
   const [distanceFromTop, setDistanceFromTop] = useState(window.scrollY) 
-  let height = 100
-  let width = 100
-  const WIDTH_SPEED = 4
-  const HEIGHT_SPEED = 2
+  const width = useRef(100)
 
   useEffect(() => {
-    function handleMove(e: Event) {
-      console.log(e)
-      if (e.deltaY > 0 && wrap.current !== null) {
-        /* wrap.current.style.transform = `translate(0px, ${wrap_position += WRAP_POS_SPEED}px)`; */
-        /* img.current.style.objectPosition = `0px ${img_position -= IMG_POS_SPEED}%` */
-        if (width > 34){
-          // eslint-disable-next-line react-hooks/exhaustive-deps
-          wrap.current.style.width = `${width -= WIDTH_SPEED}vw`
-        }
-        if (height > 90){
-          // eslint-disable-next-line react-hooks/exhaustive-deps
-          wrap.current.style.height = `${height -= HEIGHT_SPEED}vh`
-        }
-        if (height > 90){
-          wrap.current.style.margin = `${2 * (100 - height)/8}% ${(100 - width)/2}% ${6 * (100 - height)/8}% ${(100 - width)/2}%`
-        }
-      }
+    function handleMove() {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      setDistanceFromTop((_dist) => window.scrollY)
     }
+
     document.addEventListener("scroll", handleMove);
-    document
+
     return () => window.removeEventListener('scroll', handleMove)
   }, []);
+  useEffect(() => {
+    if (wrap.current !== null) {
+      if(distanceFromTop < 1000 && 100 - (distanceFromTop/12) > 34){
+        wrap.current.style.width = `${width.current = 100 - (distanceFromTop/12)}vw`
+      }
+    }
+  },[distanceFromTop])
 
   return (
     <main>
