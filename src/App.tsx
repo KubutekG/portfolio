c426bbc535833559733ca07c91d054b616f224be
KubutekG <jakubgiedzicz@gmail.com>
2023-07-28 15:55:28 +0200

    intro img wrap styling v1.1 (page load fix)(needs small viewport edgecase fix)

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,71 @@ import "../styles/main.css";
 
 function App() {
   /* const { t } = useTranslation(); */
-  const wrap = useRef<HTMLDivElement>(null)
-  const img = useRef<HTMLImageElement>(null)
-  const [distanceFromTop, setDistanceFromTop] = useState(window.scrollY) 
-  const width = useRef(100)
-  const height = useRef(100)
+  const wrap = useRef<HTMLDivElement>(null);
+  const img = useRef<HTMLImageElement>(null);
+  const [distanceFromTop, setDistanceFromTop] = useState(window.scrollY);
+  const width = useRef(100);
+  const height = useRef(100);
 
+  function setWrapSize() {
+    if (wrap.current !== null) {
+      if (100 - distanceFromTop / 12 > 34) {
+        wrap.current.style.width = `${(width.current = +(
+          100 -
+          distanceFromTop / 12
+        ).toFixed(0))}vw`;
+      } else {
+        wrap.current.style.width = "34vw";
+        width.current = 34;
+      }
+      if (100 - distanceFromTop / 60 > 87) {
+        wrap.current.style.height = `${(height.current = +(
+          100 -
+          distanceFromTop / 60
+        ).toFixed(0))}vh`;
+      } else {
+        wrap.current.style.height = "87vh";
+        height.current = 87;
+      }
+      wrap.current.style.margin = `${(1 * (100 - height.current)) / 8}% ${
+        (100 - width.current) / 2
+      }% ${(1 * (100 - height.current)) / 8}% ${(100 - width.current) / 2}%`;
+    }
+  }
+  function setWrapImgPosition(setImg: boolean, type: string, top: string) {
+    if (wrap.current !== null && img.current !== null) {
+      wrap.current.style.position = type;
+      wrap.current.style.top = top;
+      wrap.current.style.left = "0";
+      if (setImg) {
+        img.current.style.objectPosition = `50% ${50 - distanceFromTop / 50}%`;
+      }
+    }
+  }
   useEffect(() => {
     function handleMove() {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      setDistanceFromTop((_dist) => window.scrollY)
+      setDistanceFromTop((_dist) => window.scrollY);
+    }
+    document.addEventListener("scroll", handleMove);
+    setWrapSize();
+    if (distanceFromTop > 900) {
+      setWrapImgPosition(true, "absolute", "900px");
     }
 
-    document.addEventListener("scroll", handleMove);
-
-    return () => window.removeEventListener('scroll', handleMove)
+    return () => window.removeEventListener("scroll", handleMove);
   }, []);
   useEffect(() => {
     if (wrap.current !== null && img.current !== null) {
-      if(distanceFromTop <= 800 && 100 - (distanceFromTop/12) > 34){
-        wrap.current.style.width = `${width.current = +(100 - (distanceFromTop/12)).toFixed(0)}vw`
-        wrap.current.style.height = `${height.current = +(100 - (distanceFromTop/60)).toFixed(0)}vh`
-        wrap.current.style.margin = `${1 * (100 - height.current)/8}% ${(100 - width.current)/2}% ${1 * (100 - height.current)/8}% ${(100 - width.current)/2}%`
-      } else if(distanceFromTop >= 900) {
-        wrap.current.style.position = 'absolute'
-        wrap.current.style.top = `900px`
-        wrap.current.style.left = '0'
-        img.current.style.objectPosition = `50% ${50 - distanceFromTop/50}%`
-      } else if(distanceFromTop <= 900) {
-        wrap.current.style.position = 'fixed'
-        wrap.current.style.top = `0`
-        wrap.current.style.left = '0'
+      if (distanceFromTop <= 800 && 100 - distanceFromTop / 12 > 34) {
+        setWrapSize();
+      } else if (distanceFromTop >= 900) {
+        setWrapImgPosition(true, "absolute", "900px");
+      } else if (distanceFromTop <= 900) {
+        setWrapImgPosition(false, "fixed", "0px");
       }
     }
-  },[distanceFromTop])
+  }, [distanceFromTop]);
 
   return (
     <main>
@@ -59,7 +90,7 @@ function App() {
       </button>
       <section className="intro">
         <div className="intro-img-wrap" ref={wrap}>
-          <img src="/public/dummy.jpg" className="intro-img" ref={img}/>
+          <img src="/public/dummy.jpg" className="intro-img" ref={img} />
         </div>
       </section>
     </main>
