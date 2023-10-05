ced6e5be22c659902dd951faefc1ad7fdff56398
KubutekG <jakubgiedzicz@gmail.com>
2023-10-05 13:26:23 +0200

    project element refactored (excess rendering fix)

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -18,23 +18,23 @@ export default function Project({
   descriptionExt: string;
   tools: string;
   id: number;
-  link: string
+  link: string;
   isProjectOpen: number;
   setIsProjectOpen: (id: number) => void;
 }) {
-  const height = useHeight()
-  const imgHeight = +(height * 0.85 * 0.7).toFixed(0)
-  const [imgN, setImgN] = useState(0)
+  const height = useHeight();
+  const imgHeight = +(height * 0.85 * 0.7).toFixed(0);
+  const [imgN, setImgN] = useState(0);
   const styles = {
     previous: {
       top: `${imgHeight * 0.5}px`,
-      left: '2vw'
+      left: "2vw",
     },
     next: {
       top: `${imgHeight * 0.5}px`,
-      right: '2vw'
-    }
-  }
+      right: "2vw",
+    },
+  };
   useEffect(() => {
     const handleESC = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
@@ -43,39 +43,69 @@ export default function Project({
     };
     document.addEventListener("keydown", handleESC);
     return () => document.removeEventListener("keydown", handleESC);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  function handleImageSwitch(increment: boolean){
-    if (imgN == 0 && !increment){
-      setImgN(img.length - 1)
-    } else if (imgN === img.length - 1 && increment){
-      setImgN(0)
-    } else if (increment){
-      setImgN((i) => i + 1)
+  function handleImageSwitch(increment: boolean) {
+    if (imgN == 0 && !increment) {
+      setImgN(img.length - 1);
+    } else if (imgN === img.length - 1 && increment) {
+      setImgN(0);
+    } else if (increment) {
+      setImgN((i) => i + 1);
     } else {
-      setImgN((i) => i - 1)
+      setImgN((i) => i - 1);
     }
   }
-  
+
   return (
     <>
-      <div className="project" onClick={() => setIsProjectOpen(id)}>
-        <div className="project-img-desc-wrap">
-          <img src={img[imgN]} loading="lazy"/>
-          <h2>{name}</h2>
-          <h2>{description}</h2>
-        </div>
-      </div>
-      <div className={isProjectOpen === id ? "extended-shown" : "hidden"}>
-        <div className="nav" style={styles.previous} onClick={() => handleImageSwitch(false)}>{"<"}</div>
-        <div className="nav" style={styles.next} onClick={() => handleImageSwitch(true)}>{">"}</div>
-        <div className="img-counter">{imgN + 1}/{img.length}</div>
-        <img src={img[imgN]} loading="lazy"/>
-        <a href={link} target="_blank" rel="noreferrer noopener"><h2>{name}</h2></a>
-        <p>{descriptionExt}</p>
-        <p>{tools}</p>
-      </div>
+      {isProjectOpen == id ? (
+        <>
+          <div className="project" onClick={() => setIsProjectOpen(id)}>
+            <div className="project-img-desc-wrap">
+              <img src={img[imgN]} loading="lazy" />
+              <h2>{name}</h2>
+              <h2>{description}</h2>
+            </div>
+          </div>
+          <div className={isProjectOpen === id ? "extended-shown" : "hidden"}>
+            <div
+              className="nav"
+              style={styles.previous}
+              onClick={() => handleImageSwitch(false)}
+            >
+              {"<"}
+            </div>
+            <div
+              className="nav"
+              style={styles.next}
+              onClick={() => handleImageSwitch(true)}
+            >
+              {">"}
+            </div>
+            <div className="img-counter">
+              {imgN + 1}/{img.length}
+            </div>
+            <img src={img[imgN]} loading="lazy" />
+            <a href={link} target="_blank" rel="noreferrer noopener">
+              <h2>{name}</h2>
+            </a>
+            <p>{descriptionExt}</p>
+            <p>{tools}</p>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="project" onClick={() => setIsProjectOpen(id)}>
+            <div className="project-img-desc-wrap">
+              <img src={img[imgN]} loading="lazy" />
+              <h2>{name}</h2>
+              <h2>{description}</h2>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
